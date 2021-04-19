2f0a1975a34624ee6a9a6ad162a171768406e282
shubhamiitd <shubhamyadav572@gmail.com>
2021-04-19 23:29:25 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,11 +5,11 @@ bot = telebot.TeleBot("1607683994:AAGotYV7rp5cixLimS33rr0P1ir3-BBm6es")
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-	bot.send_message(message.from, "Send us the new post. Post can contain :images, texts.")
+	bot.send_message(chat_id, "Send us the new post. Post can contain :images, texts.")
 
 @bot.message_handler(content_types=['text', 'image'])
 def handle_text_image(message):
-	bot.send_message(message.from,message.text)
+	bot.send_message(chat_id,message.text)
 	print(message.text);
 	pass
 while True:
