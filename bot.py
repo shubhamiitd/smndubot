1782248fcff6d5919f26fc45a176832bc0c81667
shubhamiitd <shubhamyadav572@gmail.com>
2021-04-19 23:49:06 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,22 +15,16 @@ def handle_text_image(message):
 @bot.message_handler(content_types=['text', 'image'])
 def handle_text_image(message):
 	keyboard = telebot.types.InlineKeyboardMarkup()
-   keyboard.add(
-       telebot.types.InlineKeyboardButton(
-           ‘Message the developer’, url='telegram.me/artiomtb'
-       )
-   )
-   bot.send_message(
-       message.chat.id,
-       '1) To receive a list of available currencies press /exchange.\n' +
-       '2) Click on the currency you are interested in.\n' +
-       '3) You will receive a message containing information regarding the source and the target currencies, ' +
-       'buying rates and selling rates.\n' +
-       '4) Click “Update” to receive the current information regarding the request. ' +
-       'The bot will also show the difference between the previous and the current exchange rates.\n' +
-       '5) The bot supports inline. Type @<botusername> in any chat and the first letters of a currency.',
-       reply_markup=keyboard
-   )
+   	keyboard.add(telebot.types.InlineKeyboardButton(‘Message the developer’, url='telegram.me/artiomtb'))
+   	bot.send_message(message.chat.id,
+       	'1) To receive a list of available currencies press /exchange.\n' +
+       	'2) Click on the currency you are interested in.\n' +
+       	'3) You will receive a message containing information regarding the source and the target currencies, ' +
+       	'buying rates and selling rates.\n' +
+       	'4) Click “Update” to receive the current information regarding the request. ' +
+       	'The bot will also show the difference between the previous and the current exchange rates.\n' +
+       	'5) The bot supports inline. Type @<botusername> in any chat and the first letters of a currency.',
+       	reply_markup=keyboard)
 	pass
 
 
