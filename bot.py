240c87ebec905bd9154e1832667c27d03dae6af0
shubhamiitd <shubhamyadav572@gmail.com>
2021-04-20 12:51:46 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,13 +10,26 @@ def send_welcome(message):
         message.chat.id, "Send us the new post. Post can contain :images, text."
     )
 
+@bot.message_handler(content_types=["text"])
+def set_buttons():
+    msgg=message.text;
+    msgg=msgg.split('|');
+    global button
+    global button2
+    button = types.InlineKeyboardMarkup()
+    btn1 = types.InlineKeyboardButton(msgg[0], callback_data='/send')
+    btn2 = types.InlineKeyboardButton(msgg[1], callback_data='/email')
+    button.row(btn1, btn2)
+    button2 = types.InlineKeyboardMarkup()
+    btn3 = types.InlineKeyboardButton(i18n.t('bot.btn3'), callback_data='/as_is')
+    btn4 = types.InlineKeyboardButton(i18n.t('bot.btn4'), callback_data='/converted')
+    button2.row(btn3, btn4) 
 """
 @bot.message_handler(content_types=["text", "image"])
 def handle_text_image(message):
     bot.send_message(message.chat.id, message.text)
     print(message.chat.id)
     pass
-"""
 
 @bot.message_handler(commands=["help"])
 def handle_text_image(message):
@@ -37,6 +50,7 @@ def handle_text_image(message):
         + "5) The bot supports inline. Type @<botusername> in any chat and the first letters of a currency.",
         reply_markup=keyboard,
     )
+"""
 
 
 while True:
