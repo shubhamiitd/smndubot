5a961033faa8d26d5972e5d684181e94a528d7bc
shubhamiitd <shubhamyadav572@gmail.com>
2021-04-21 20:38:13 +0530

    Update data.py

--- data.py
+++ data.py
@@ -4,15 +4,26 @@ import mysql.connector
 db = mysql.connector.connect(host="localhost",user="root",password="you",database="smndubot")
 
 cursor = db.cursor()
+cursor.execute("DROP TABLE IF EXISTS users")
 
-# Create table as per requirement
 sql = """CREATE TABLE `users` (
+	'firstname' TEXT DEFAULT NULL,
 	`username` TEXT DEFAULT NULL,
 	`user_id` INT,
 	`is_admin` TINYINT DEFAULT '0',
 	`last_access` TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );"""
 
+cursor.execute(sql)
+sql = """INSERT INTO users(firstname, username,
+         user_id, is_admin)
+         VALUES ('Shubham','sy_tele', 793347625, 1 )"""
+
+  
+# loop through the rows
+for row in result:
+    print(row)
+    print("\n")
 try:
    # Execute the SQL command
    cursor.execute(sql)
@@ -22,7 +33,11 @@ except:
    # Rollback in case there is any error
    db.rollback()
 
-
+# execute your query
+cursor.execute("SELECT * FROM users")
+  
+# fetch all the matching rows 
+result = cursor.fetchall()
 
 
 
