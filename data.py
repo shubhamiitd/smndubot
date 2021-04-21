ccf5571321cca59ca22a775927fcd0e1458d1647
shubhamiitd <shubhamyadav572@gmail.com>
2021-04-21 19:56:47 +0530

    Create data.py

---
+++ data.py
@@ -0,0 +1,29 @@
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="you"
+  )
+
+mycursor = mydb.cursor()
+mycursor.execute("SHOW DATABASES")
+
+for x in mycursor:
+  print(x)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
