89eafd968482f0691fdb73fcd6fdab26294beaac
shubhamiitd <shubhamyadav572@gmail.com>
2021-04-21 19:58:38 +0530

    Update data.py

--- data.py
+++ data.py
@@ -1,9 +1,11 @@
 
+import mysql.connector
+
 mydb = mysql.connector.connect(
   host="localhost",
   user="root",
   password="you"
-  )
+ )
 
 mycursor = mydb.cursor()
 mycursor.execute("SHOW DATABASES")
