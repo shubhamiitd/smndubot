6fefe468e85e7d223a6a13dfdfff8a7bd389d3bb
shubhamiitd <shubhamyadav572@gmail.com>
2021-04-22 23:40:41 +0530

    Update smndubot.py

--- smndubot.py
+++ smndubot.py
@@ -3,7 +3,7 @@ from telebot import types
 
 bot = telebot.TeleBot("1607683994:AAGotYV7rp5cixLimS33rr0P1ir3-BBm6es")
 
-def del(message):
+def deletemsg(message):
   k= message.document.mime_type;
   k=k.split("/");
   if (k[0]=="application"):
@@ -12,12 +12,18 @@ def del(message):
     return false
 
 # Handles all sent documents and audio files
-@bot.message_handler(content_types=['document', 'audio'])
-def handle_docs_audio(message):
-  f=del(message);
+@bot.message_handler(content_types=['document'])
+def handle_docs(message):
+  f=deletemsg(message)
   if(f==true):
-    bot.deleteMessage(message.chat.id, message.id);
+    bot.deleteMessage(message.chat.id, message.id)
 	pass
 
 
 bot.polling()
+
+while True:
+    try:
+        bot.polling()
+    except Exception:
+        time.sleep(15)
